ea8b59e725fb632431fe937fe0b8d006e493e13b
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2023-04-27 12:03:36 +0530

    sanity

---
+++ main.js
@@ -0,0 +1,9 @@
+    //Function demonstrating the Href Method
+    
+    function hrefFunction(){
+        window.location.href = "/Cart.html";
+    }
+
+    function LoginFunction(){
+        window.location.href = "/Login.html";
+    }
